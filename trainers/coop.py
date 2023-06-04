46b8e0d6a5911aeed9bf697ac0580cf49e4f4818
Kexin Gao <changlin.jiang2017@gmail.com>
2023-06-04 00:50:58 -0700

    cpkt: more debug log #3

--- trainers/coop.py
+++ trainers/coop.py
@@ -84,7 +84,7 @@ class TextEncoder(nn.Module):
 
         # x.shape = [batch_size, n_ctx, transformer.width]
         # take features from the eot embedding (eot_token is the highest number in each sequence)
-        print(tokenized_prompts)
+        print(tokenized_prompts[:30])
         print(tokenized_prompts.argmax(dim=-1))
         x = x[torch.arange(x.shape[0]), tokenized_prompts.argmax(dim=-1)] @ self.text_projection
 
@@ -132,7 +132,6 @@ class PromptLearner(nn.Module):
         print(f"Number of context words (tokens): {n_ctx}")
 
         self.ctx = nn.Parameter(ctx_vectors)  # to be optimized
-        print("ctx vector:\n ", self.ctx)
 
         classnames = [name.replace("_", " ") for name in classnames]
         # name_lens = [len(_tokenizer.encode(name)) for name in classnames]
@@ -162,6 +161,7 @@ class PromptLearner(nn.Module):
 
     def forward(self):
         ctx = self.ctx
+        print("ctx vector:\n ", self.ctx)
         if ctx.dim() == 2:
             ctx = ctx.unsqueeze(0).expand(self.n_cls, -1, -1)
 
